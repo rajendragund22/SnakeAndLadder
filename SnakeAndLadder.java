40120b13e7cc0fc94efd5f484cf68f73323b0ee8
RajendraGund <rajendragund2015@gmail.com>
2021-08-25 17:26:17 +0530

    Added : Position at Zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,8 +3,9 @@ package com.snakel_adder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-	 System.out.println("Welcome into Snake and Ladder program !!");
-
+		int Position = 0;
+		System.out.println("Welcome into Snake and Ladder program !!");
+		System.out.println("Your Starting Position is : " + Position);
 	}
 
 }
