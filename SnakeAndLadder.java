1333dcf730b4f544a2cf54357d9d0df5ec317b4a
RajendraGund <rajendragund2015@gmail.com>
2021-08-25 16:55:27 +0530

    Welcome meassage for user

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.snakel_adder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+	 System.out.println("Welcome into Snake and Ladder program !!");
+
+	}
+
+}
