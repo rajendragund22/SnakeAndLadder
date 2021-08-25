04db4b76497918fafc1c836451c4f39e03f2f11c
RajendraGund <rajendragund2015@gmail.com>
2021-08-25 17:38:46 +0530

    Added: Roll the dice to get between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,6 +6,11 @@ public class SnakeAndLadder {
 		int Position = 0;
 		System.out.println("Welcome into Snake and Ladder program !!");
 		System.out.println("Your Starting Position is : " + Position);
+		Dice();
+	}
+	public static void Dice(){
+		int Dice = (int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("Your Dice Value is : " +Dice);
 	}
 
 }
